77ccf0ab555e211c70e40c451b39da9fcf7fe71b
Renan Bonani <bonani.silva@gmail.com>
2021-06-16 16:00:35 -0300

    iniciando Projeto

---
+++ springboot-backend/src/main/java/crud/springbootbackend/model/Employee.java
@@ -0,0 +1,82 @@
+package crud.springbootbackend.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "employees")
+public class Employee {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	@Column(name = "name")
+	private String name;
+	@Column(name = "cpf")
+	private String cpf;
+	@Column(name = "birthday")
+	private String birthday;
+	@Column(name = "email_id")
+	private String email;
+	@Column(name = "name")
+	private String phone;
+		
+
+	public Employee() {
+		
+		
+	}
+	
+	
+	public Employee(String name, String cpf, String birthday, String email, String phone) {
+		super();
+		this.name = name;
+		this.cpf = cpf;
+		this.birthday = birthday;
+		this.email = email;
+		this.phone = phone;
+	}
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getBirthday() {
+		return birthday;
+	}
+	public void setBirthday(String birthday) {
+		this.birthday = birthday;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	
+}
